6b3a93ee964d9692bdefa8a7685de13c643ea513
wolf-mtwo <wolf.mtwo@outlook.com>
2020-12-01 01:21:57 -0400

    Adding html client

--- src/modules/home/service.js
+++ src/modules/home/service.js
@@ -1,12 +1,15 @@
 const config = require('../../../package.json');
+const path = require('path');
 
 class Service {
 
   static home(req, res, next) {
-    res.json({
-      vesion: '0.0.0',
-      name: config.name
-    });
+    // res.json({
+    //   vesion: '0.0.0',
+    //   name: config.name
+    // });
+    //res.render('index', 'html');
+    res.sendFile(path.join(__dirname + '../../../../public/index.html'));
   }
 }
 
